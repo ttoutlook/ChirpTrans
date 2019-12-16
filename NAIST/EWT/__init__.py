6ed23c8f465180c5de077d86e8d89338454a3d42
Code <tangtaooo123@gmail.com>
2019-12-16 21:57:21 +0900

    edit

---
+++ NAIST/EWT/__init__.py
@@ -0,0 +1,6 @@
+'''
+@author: Tang Tao
+@contact: tangtaooo123@gmail.com
+@file: __init__.py.py
+@time: 12/16/2019 9:25 PM
+'''
--- python/NAIST/EWT/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-@author: Tang Tao
-@contact: tangtaooo123@gmail.com
-@file: __init__.py.py
-@time: 12/16/2019 9:25 PM
-'''
